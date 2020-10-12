638b2223725e3269d4373340f8b5c03f35221e59
brunosana <brunosa@dcomp.ufs.br>
2020-10-12 20:15:50 -0300

    SignIn with API working

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useCallback } from 'react';
+import React, {
+  createContext, useCallback, useContext, useState,
+} from 'react';
 
 import api from '../services/api';
 
@@ -8,21 +10,46 @@ interface signInCredentials{
 }
 
 interface AuthContextData{
-    name: string;
+    user: object;
     signIn(credentials: signInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+interface AuthState {
+    token: string;
+    user: object;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBerber:token');
+    const user = localStorage.getItem('@GoBerber:user');
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('/sessions', { email, password });
-    console.log(response.data);
+    const { token, user } = response.data;
+    localStorage.setItem('@GoBerber:token', token);
+    localStorage.setItem('@GoBerber:user', JSON.stringify(user));
+    setData({ token, user });
   }, []);
   return (
 
-    <AuthContext.Provider value={{ name: 'Sana', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
+};
+
+export const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
